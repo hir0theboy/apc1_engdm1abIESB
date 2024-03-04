7b94c9814d6e641f1ea39ec82c05e9705f1dbfb5
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-03-04 13:10:50 +0000

    adicionando mais tipos

---
+++ aulas/aula03/tipos/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+  char caracter; // caracteres ' ' (apenas um caractere)
+  int inteiro; // inteiros
+  float decimal6; // reais com 6 casas
+  double decimal15; // reais com 15 casas 
+  void sem_valor; // sem valor 
+  signed char caracter_com_sinal; // caracteres com sinal
+  unsigned char caracter_sem_sinal;
+
+  signed int inteiro_com_sinal;
+  unsigned int inteiro_sem_sinal;
+
+  short int inteiro_curto;
+  long int inteiro_longo;
+
+  long double duplo_longo;
+
+  signed short int inteiro_curto_com_sinal;
+  unsigned short int inteiro_curto_sem_sinal;
+  signed long int inteiro_longo_com_sinal
+  unsigned long int inteiro_longo_sem_sinal;
+  signed long double duplo_longo_com_sinal;
+  unsigned long double duplo_longo_sem_sinal;
+  
+  //nao possui string, porem aceita "uma frase"
+  // cada tipo usa um espaço de memoria diferente
+  // criar variavel: tipo nome_da_variavel;
+
+  
+  return 0;
+}
