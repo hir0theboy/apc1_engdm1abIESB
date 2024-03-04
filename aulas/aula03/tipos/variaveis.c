dd37f05c8c7d374a3ac26bb7e777b042e5bc173f
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-03-04 13:17:09 +0000

    add variaveis.c

---
+++ aulas/aula03/tipos/variaveis.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+int main() {
+
+  int numero;
+
+  numero = 10;
+
+  numero = 100; //ele pode ser mudado por ser uma variavel
+
+  const float pi = 3,14  //constante nao pode ser mudada por ser uma constante
+
+    int sexo = 1;
+    const int sexo_masculino = 1;
+
+  sexo = sexo_masculino; //valor fixo = constante (const)
+
+  
+
+    
+  return 0;
+}
