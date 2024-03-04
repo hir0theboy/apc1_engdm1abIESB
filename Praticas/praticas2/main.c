fe1df3aee2c07d5df472a0e4f880f87cc6441fa8
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-03-04 13:50:57 +0000

    terminando questao 4

--- Praticas/praticas2/main.c
+++ Praticas/praticas2/main.c
@@ -8,12 +8,21 @@
   float preco_final;
    const float ICMS = 0.17;
    const float COFINS = 0.076;
-   const float pis_pasep = 0.0165;
+   const float PIS_PASEP = 0.0165;
 
    preco_inicial = 100.0f;
-   preco_final = (1 + ICMS + COFINS + pis_pasep) * preco_inicial;
+   float valor_icms = preco_inicial * ICMS;
+   float valor_cofins = preco_inicial * COFINS;
+   float valor_pis_pasep = preco_inicial * PIS_PASEP;
+   
+   
+   preco_final = (1 + ICMS + COFINS +  PIS_PASEP) * preco_inicial;
 
+     printf( "O preco inicial do produto eh: %f\n", preco_inicial);
      printf( "O preco final do produto eh: %f\n", preco_final);
+     printf( "O valor do ICMS eh: %f\n", valor_icms);
+     printf( "O valor do COFINS eh: %f\n", valor_cofins);
+     printf( "O valor do PIS/PASEP eh: %f\n", valor_pis_pasep);
    
    return 0;
  }
--- aulas/aula03/tipos/variaveis.c
+++ aulas/aula03/tipos/variaveis.c
@@ -8,7 +8,7 @@ int main() {
 
   numero = 100; //ele pode ser mudado por ser uma variavel
 
-  const float pi = 3,14  //constante nao pode ser mudada por ser uma constante
+  const float PI = 3,14  //constante nao pode ser mudada por ser uma constante
 
     int sexo = 1;
     const int sexo_masculino = 1;
@@ -17,7 +17,7 @@ int main() {
 
    #define PI 3,14f; 
   //define uma constante
-
+  //constante nao pode ser minuscula
     
   return 0;
 }
