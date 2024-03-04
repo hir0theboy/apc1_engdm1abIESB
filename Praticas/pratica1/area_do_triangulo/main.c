3456341f3cf725b70e63cd27ce5c1190a4db1ff9
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-03-04 12:20:13 +0000

    primeiro commit

--- Praticas/pratica1/area_do_triangulo/main.c
+++ Praticas/pratica1/area_do_triangulo/main.c
@@ -2,7 +2,22 @@
 
 int main() {
 
+    //entrada
   
+  float base;
+  float altura;
+
+  
+  base = 2.0f; // 2.0f (float) 2.0 (double)
+  altura = 3.0f;
+
+    //processamento
+
+float area = base*altura/2; //armazenando resultado em uma variavel
+
+    //saida
+  
+  printf("A area do triangulo eh: %f\n", area);
   
 return 0;
 }
