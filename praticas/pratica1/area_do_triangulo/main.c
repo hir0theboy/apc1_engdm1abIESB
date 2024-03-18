f2596022f20843817b8c48ef2560c356cda09719
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-03-18 12:17:26 +0000

    adicionando scanf

--- praticas/pratica1/area_do_triangulo/main.c
+++ praticas/pratica1/area_do_triangulo/main.c
@@ -11,6 +11,12 @@ int main() {
   base = 2.0f; // 2.0f (float) 2.0 (double)
   altura = 3.0f;
 
+  printf("entre com o valor da base: ");
+  int deu_certo = scanf("%f", &base);
+
+  printf("entre com o valor da altura: ");
+  deu_certo = scanf("%f", &altura);
+
     //processamento
 
 float area = base*altura/2; //armazenando resultado em uma variavel
