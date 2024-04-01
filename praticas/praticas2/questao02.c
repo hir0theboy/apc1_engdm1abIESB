bcce187568865ed549c2bd9cd0afb7f9cfd69ec8
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-04-01 13:39:01 +0000

    questao2

--- praticas/praticas2/questao02.c
+++ praticas/praticas2/questao02.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main() {
+int numero1;
+int numero2;
 
+  printf("entre com o primeiro numero: ");
+  int deu_certo = scanf("%i", &numero1);
+
+  printf("entre com o segundo numero: ");
+  deu_certo = scanf("%i", &numero2);
+
+  int divisao = numero1 / numero2;
+  int resto = numero1 % numero2;
+
+  printf("o resultado da divisao eh %i e o resto eh %i\n",divisao, resto);
+  
+
+    
 
 
 
