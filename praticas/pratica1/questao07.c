b6e86bf541318d2799f762e759e2e86fb856f984
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-04-01 13:48:23 +0000

    questao 07 p1

--- praticas/pratica1/questao07.c
+++ praticas/pratica1/questao07.c
@@ -4,6 +4,20 @@
 
 int main() {
 int angulo;
+int altura;
+int distancia;
+
+  printf("entre com o valor do angulo: ");
+    int deu_certo = scanf("%i", &angulo);
+
+  printf("entre com o valor da distancia em metros: ");
+  deu_certo = scanf("%i", &distancia);
+
+  altura = distancia * sin(angulo);
+
+  printf("o valor da altura em metros eh %i\n", altura);
+  
+  
 
   
 
