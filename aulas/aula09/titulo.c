e29cb793c9dfaa537f496bafa40e90aa8e028c3f
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-04-29 12:11:35 +0000

    aula 9.1

--- aulas/aula09/titulo.c
+++ aulas/aula09/titulo.c
@@ -6,14 +6,17 @@ int main () {
 
   printf("Entre com a idade: ");
   int deu_certo = scanf("%d", &idade);
-
-  if (idade < 16) {
-    printf("Não pode votar\n");
-  } else if (idade >= 18 && idade < 65) {
-    printf("voto obrigatório\n");
-  } else {
-    printf("o voto é opcional\n");
-  }
+ if (deu_certo) {
+   if (idade < 16) {
+     printf("Não pode votar\n");
+   } else if (idade >= 18 && idade < 65) {
+     printf("voto obrigatório\n");
+   } else {
+     printf("o voto é opcional\n");
+   }
+} else {
+   printf("A idade inserida é invalida!\n");
+ }
 
 
   return 0;
