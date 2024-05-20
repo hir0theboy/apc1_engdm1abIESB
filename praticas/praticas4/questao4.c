7f7dd6e390cb53e4810ae785950a70e12d91292c
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-05-20 13:37:55 +0000

    aula 11.3

---
+++ praticas/praticas4/questao4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char frase [31];
+
+  printf("entre com uma frase de ate 30 caracteres: ");
+  int deu_certo = scanf("%[^\n]s", frase);
+
+  char matriz[6][5];
+
+
+
+  return 0;
+}
