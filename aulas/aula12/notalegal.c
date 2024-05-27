942652d684053d277823c934362eb08ea42d0244
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-05-27 13:35:07 +0000

    aula12.2

--- aulas/aula12/notalegal.c
+++ aulas/aula12/notalegal.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int main() {
 struct item_notafiscal_t {
   char nome[21];
   int quantidade;
   float valor;
+  float preco;
     };
   
-struct item_notafiscal_t item;
-
+struct item_notafiscal_t item[5];
+  float total = 0;
+  
 for (int i=0; i<5; i++) {
   printf("entre com o nome do produto: ");
-  int deu_certo = scanf("%[^\n]s", item.nome);
+  int deu_certo = scanf("%[^\n]s", item[i].nome);
   while(getchar() != '\n');
-
+  int tamanho = strlen(item[i].nome);
+  for (int j =0; j<tamanho; j++) {
+    item[i].nome[j] = toupper(item[i].nome[j]);
+    
+  }
   printf("Entre com a quantidade: ");
-  deu_certo = scanf("%i", &item.quantidade);
+  deu_certo = scanf("%i", &item[i].quantidade);
   while(getchar() != '\n');
 
   printf("Entre com o valor do produto: ");
-  deu_certo = scanf("%f", &item.valor);
+  deu_certo = scanf("%f", &item[i].preco);
   while(getchar() != '\n');
+
+  item[i].valor = item[i].quantidade * item[i].preco;
+  total = total + item[i].valor;
 
 }
   printf("\n N O T A  F I S C A L\n");
-  printf("Item       Qtd    Valor\n");
+  printf("Item       Qtd    Valor     TOTAL\n");
   for (int i = 0; i < 5; i++) {
-  printf("%-10s %03i %8.2f\n", item.nome, item.quantidade, item.valor);
+  printf("%-10s %03i %8.2f %8.2f\n", item[i].nome, item[i].quantidade, item[i].preco,
     item[i].valor);
   }
+
+  printf("-------------------------------\n");
+  printf("Total: %26.2f\n", total);
+  
 
   return 0;
 }
