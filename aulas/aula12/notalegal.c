dabc93ad14ef083cf36afb82838881d2176f0d6e
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-05-27 12:25:17 +0000

    aula12

---
+++ aulas/aula12/notalegal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+struct item_notafiscal_t {
+  char nome[21];
+  int quantidade;
+  float valor;
+    };
+  
+struct item_notafiscal_t item;
+
+printf("entre com o nome do produto: ");
+  int deu_certo = scanf("%[^\n]s", item.nome);
+  while(getchar() != '\n');
+
+  printf("Entre com a quantidade: ");
+  deu_certo = scanf("%i", &item.quantidade);
+  while(getchar() != '\n');
+  
+  printf("Entre com o valor do produto: ");
+  deu_certo = scanf("%f", &item.valor);
+  while(getchar() != '\n');
+
+  printf("\n N O T A  F I S C A L\n");
+  printf("Item       Qtd    Valor\n");
+  printf("%-10s %03i %8.2f\n", item.nome, item.quantidade, item.valor);
+
+  return 0;
+}
